d21d5271b886dd19d52e45fde8d2d28a0ade2a9d
shuyih127 <shu.yih@northeastern.edu>
2024-09-19 00:34:32 -0400

    Complete the Lab

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import Labs from "./Labs";
+import Kanbas from './Kanbas';
+import { HashRouter, Link, Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <h1>Welcome to Web Dev</h1>
-      <Labs />
+        <HashRouter>
+          <div>
+            <Routes>
+              <Route path="/" element={<Navigate to="Labs" />} />
+              <Route path="/Labs/*" element={<Labs />} />
+              <Route path="/Kanbas/*" element={<Kanbas />} />
+            </Routes>
+          </div>
+        </HashRouter>  
     </div>
   );
 }
---
+++ src/Kanbas/index.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function Kanbas() {
+    return (
+      <div id="wd-kanbas">
+        <h1>Kanbas</h1> 
+        <ul>
+            <li><Link to="/Labs">Landing Page</Link></li>
+        </ul>
+      </div>
+  );}
+  
