ed5e9a9000c51504329335769e31f31737ac0e1f
shuyih127 <shu.yih@northeastern.edu>
2024-12-05 17:53:48 -0500

    a6 mongo

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,36 +1,16 @@
 import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
-
 export default function AccountNavigation() {
-  const { currentUser } = useSelector((state: any) => state.accountReducer);
-  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
-  const { pathname } = useLocation();
-
-  return (
-    <div id="wd-account-navigation" className="list-group fs-5 rounded-0">
-      {links.includes("Signin") && (
-        <Link
-          to="/Kanbas/Account/Signin"
-          className={`list-group-item border-0 ${pathname === "/Kanbas/Account/Signin" ? "active" : ""}`}
-        >
-          Signin
-        </Link>
-      )}
-      {links.includes("Signup") && (
-        <Link
-          to="/Kanbas/Account/Signup"
-          className={`list-group-item border-0 ${pathname === "/Kanbas/Account/Signup" ? "text-danger" : ""}`}
-        >
-          Signup
-        </Link>
-      )}
-      {links.includes("Profile") && (
-        <Link
-          to="/Kanbas/Account/Profile"
-          className={`list-group-item border-0 ${pathname === "/Kanbas/Account/Profile" ? "text-danger" : ""}`}
-        >
-          Profile
-        </Link>
-      )}
-    </div>
-  );}
+ const { currentUser } = useSelector((state: any) => state.accountReducer);
+ const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+ const active = (path: string) => (pathname.includes(path) ? "active" : "");
+ const { pathname } = useLocation();
+ return (
+   <div id="wd-account-navigation" className="list-group">
+     {links.map((link) => (
+       <Link key={link} to={`/Kanbas/Account/${link}`} className={`list-group-item ${active(link)}`}> {link} </Link>
+     ))}
+     {currentUser && currentUser.role === "ADMIN" && (
+       <Link to={`/Kanbas/Account/Users`} className={`list-group-item ${active("Users")}`}> Users </Link> )}
+   </div>
+);}
